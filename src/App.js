37d7b31357b58d6a7e60fe6bc8030687c5d88d6b
akshat <akshat_g@ch.iitr.ac.in>
2020-09-20 02:59:05 +0530

    Added func to edit the text editor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Editor from "./Editor"
 
 function App() {
+  const [html,sethtml]=useState("")
+  const [css,setcss]=useState("")
+  const [js,setjs]=useState("")
   return (
+   
     <>
 
     <div className="editors">
-           <Editor/>
-           <Editor/>
-           <Editor/>
+           <Editor
+           language="xml"
+           title="HTML"
+           value={html}
+           onChange={sethtml}
+           />
+           <Editor
+           language="css"
+           title="CSS"
+           value={css}
+           onChange={setcss}
+           />
+           <Editor
+           language="javascript"
+           title="JS"
+           value={js}
+           onChange={setjs}
+           />
+         
       </div>
       <div className="document">
         <iframe
