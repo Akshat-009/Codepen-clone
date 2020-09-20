6c28b04951e5fcd35c711e4ff7512a16d5178446
akshat <akshat_g@ch.iitr.ac.in>
2020-09-20 13:24:20 +0530

    Added the basic functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import Editor from "./Editor"
 
 function App() {
   const [html,sethtml]=useState("")
   const [css,setcss]=useState("")
   const [js,setjs]=useState("")
+  const [srcDoc,setsrcDoc]=useState("")
+  useEffect(()=>{
+    const timeout=()=>{
+       setsrcDoc(`<html>
+      <body>
+      ${html}
+      </body>
+      <style>
+      ${css}
+      </style>
+      <script>
+      ${js}
+     </script>
+    </html>`)
+    }  
+    timeout()
+  
+
+  },[html,css,js])
+ 
   return (
    
     <>
@@ -32,6 +52,7 @@ function App() {
       </div>
       <div className="document">
         <iframe
+        srcDoc={srcDoc}
         title="Output"
         height="100%"
         sandbox="allow-scripts"
